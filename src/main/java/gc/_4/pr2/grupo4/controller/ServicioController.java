42c2d3675ed1aeb0a78c84c5c7c88917c8c66708
kuro2020-cod <kytian_ata@hotmail.com>
2024-09-19 22:31:56 -0300

    se agrega el metodo PUT

--- src/main/java/gc/_4/pr2/grupo4/controller/ServicioController.java
+++ src/main/java/gc/_4/pr2/grupo4/controller/ServicioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,6 +44,10 @@ public class ServicioController {
 	void borrarServicio (@PathVariable("id")  Long id) {
 		service.deleteById(id);
 		
+	}
+	@PutMapping("/servicios")
+	public Servicio modificaNuevoServicio(@RequestBody Servicio servicioDesdeService ) {
+		return service.save(servicioDesdeService);
 	}
 	
 	
