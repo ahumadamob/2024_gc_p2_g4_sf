d96bb87c0de2ded9e1705e3ab1988586607f90d5
SantiagoGonzalez <gonzalezravanelli@gmail.com>
2024-10-18 14:37:05 -0300

    Persistencia Avanzada entidad Servicio

---
+++ src/main/java/gc/_4/pr2/grupo4/dto/ResponseDto.java
@@ -0,0 +1,40 @@
+package gc._4.pr2.grupo4.dto;
+
+
+
+public class ResponseDto<T> {
+	
+	
+	//Este atributo esta encapsulado, ya que solo podremos acceder a el a travez de sus getters and Setters
+	private boolean estado;
+	private String message;
+	private T data;
+	
+	
+	
+	//Este es el metodo get del encapsulamiento de estado
+	public boolean isEstado() {
+		return estado;
+	}
+	
+	//Este es el metodo set de encapsulamiento de estado
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	
+	
+	
+
+}
